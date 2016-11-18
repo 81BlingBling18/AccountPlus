72390e17546edbae8e26cb02b027cf9429a14011
qimeng <81BlingBling18@gmail.com>
2016-11-19 00:03:05 +0800

    test

--- app/src/main/java/com/happycoding/uniquehust/accountplus/database/AccountDatabaseContract.java
+++ app/src/main/java/com/happycoding/uniquehust/accountplus/database/AccountDatabaseContract.java
@@ -11,8 +11,12 @@ public final class AccountDatabaseContract {
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_AMOUNT = "amount";
         public static final String COLUMN_NAME_DESCRIPTION = "description";
-        public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
+        public static final String COLUMN_NAME_YEAR = "year";
+        public static final String COLUMN_NAME_MONTH = "month";
+        public static final String COLUMN_NAME_DAY = "day";
         public static final String COLUMN_NAME_PIC_TIMESTAMP = "pic_timestamp";
         public static final String COLUMN_NAME_ICON_ID = "icon_id";
+        public static final String COLUMN_NAME_ISINCOME = "is_income";
     }
+
 }
